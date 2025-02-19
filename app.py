d82c7cc0f84dbba845ec08448f5f1f0d30b27e0b
rtrinu <rbinu2004@gmail.com>
2025-02-19 16:54:59 +0000

    Second Commit

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "hello world"
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return 'The About Page'
+
+@app.route('/blog')
+def blog():
+    return render_template('index.html')
+
+@app.route('/blog/<blog_id>')
+def blogpost(blog_id):
+    return f'This is another post {blog_id}'
+
+
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
